841faec38c3ceb457e829c6ad19081046f1e2913
TurkeyMcMac <jwmhjwmh@gmail.com>
2017-05-27 13:40:25 -0400

    reworked quests yet again

--- src/textadv/base/quests/Quest.java
+++ src/textadv/base/quests/Quest.java
@@ -1,82 +1,84 @@
-package textadv.base.events;
+package textadv.base.quests;
 
-public class AutoQuest implements Quest {
+import java.util.function.Consumer;
+
+import textadv.base.outfits.Describable;
+import textadv.base.world.Grid;
+
+public class Quest implements Describable {
+	
+	public enum Status {
+		FINISHED, FAILED, ACTIVE, INACTIVE
+	}
 	
 	private String name;
 	private String info;
+	private Grid grid;
 	private Status status;
-	private Event start;
-	private Event end;
-	private Event fail;
+	private Consumer<Grid> start;
+	private Consumer<Grid> end;
+	private Consumer<Grid> fail;
 	
-	public AutoQuest(String name, String info, Event start, Event end, Event fail) {
+	public Quest(String name,
+				 String info,
+				 Grid grid,
+				 Consumer<Grid> start,
+				 Consumer<Grid> end,
+				 Consumer<Grid> fail
+	) {
 		this.name = name;
 		this.info = info;
+		this.grid = grid;
 		this.start = start;
 		this.end = end;
 		this.fail = fail;
 		this.status = Status.INACTIVE;
 	}
 
-	@Override
 	public String getName() {
 		return name;
 	}
 
-	@Override
 	public String getInfo() {
 		return info;
 	}
 
-	@Override
 	public boolean start() {
 		if (status == Status.INACTIVE) {
+			start.accept(grid);
 			status = Status.ACTIVE;
-			return start.isMet();
+			return true;
 		}
 		return false;
 	}
 
-	@Override
 	public boolean end() {
 		if (status == Status.ACTIVE) {
+			end.accept(grid);
 			status = Status.FINISHED;
-			return end.isMet();
+			return true;
 		}
 		return false;
 	}
 
-	@Override
 	public boolean fail() {
 		if (status == Status.ACTIVE) {
+			fail.accept(grid);
 			status = Status.FAILED;
-			return fail.isMet();
+			return true;
 		}
 		return false;
 	}
 	
-	@Override
 	public void reset() {
 		status = Status.INACTIVE;
 	}
 	
-	@Override
 	public void setStatus(Status status) {
 		this.status = status;
 	}
-
-	@Override
+	
 	public Status getStatus() {
-		return status;
-	}
-	
-	
-	public Status checkStatus() {
-		if (!	 start()) {
-			if (!end()) {
-				 fail();
-			}
-		}
 		return status;
 	}
 	
