11e8b95333348f19751853f9c155105996eb23f9
TurkeyMcMac <jwmhjwmh@gmail.com>
2017-06-04 11:54:27 -0400

    reorganized world initialization

--- src/textadv/main/Main.java
+++ src/textadv/main/Main.java
@@ -8,18 +8,19 @@ import textadv.ui.UserInterface;
 
 public final class Main {
 	
-	@SuppressWarnings("serial")
-	private static Grid world = new Grid(100, 100, Tile.empty()) {{
+	private static Grid world = new Grid(100, 100, Tile.empty());
+	
+	private static void buildWorld() {
 		Player player = new Player(CarDir.NORTH);
-		drop(player, 30, 30);
-		setPlayer(player);
-		drop(new Stick(), 31, 30);
-		drop(new Stick(), 31, 30);
-		drop(new Hat(), 29, 30);
-		drop(new Block(), 29, 28);
-		drop(new Goblin(CarDir.NORTH), 25, 23);
-		place(new Wall("Wall", "A solid wall.", '#'), 30, 29);
-	}};
+		world.drop(player, 30, 30);
+		world.setPlayer(player);
+		world.drop(new Stick(), 31, 30);
+		world.drop(new Stick(), 31, 30);
+		world.drop(new Hat(), 29, 30);
+		world.drop(new Block(), 29, 28);
+		world.drop(new Goblin(CarDir.NORTH), 25, 23);
+		world.place(new Wall("Wall", "A solid wall.", '#'), 30, 29);
+	}
 	
 	private static UserInterface ui = new UserInterface(world);
 	
@@ -28,8 +29,9 @@ public final class Main {
 	}
 	
 	public static void main(String[] args) {
+		buildWorld();
 		screen("welcome");
-		while (true) { 
+		while(true) { 
 			screen(System.console().readLine());
 		}
 	}
